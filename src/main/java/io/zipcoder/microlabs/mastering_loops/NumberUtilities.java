6154a0c8d1f9eb08743f025109106ef38176f873
santos <santoslopez890@gmail.com>
2023-06-30 15:09:18 -0400

    work is being done

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,44 +3,90 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-
-        for (int i = start; i < stop; i++) {
+    String result ="";
+        for (int i = start; i < stop; i++) {//looping through numbers
             if (i % 2 == 0) {
-
-
+                result +=String.format("%d",i);
             }
+
         }
-        return null;
+        return result;
     }
 
-//user gives 3
-    //1+2+3=6
-    //10
-    //
+
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result ="";
+        for (int i = start; i < stop; i++) {//looping through numbers
+            if (i % 2 == 1) {
+                result +=String.format("%d",i);
+            }
+
+        }
+        return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result ="";
+        int result2=1;
+        int squarednums=0;
+        for (int i = start; i < stop; i += step) {//looping through numbers
+
+                squarednums = i*i;//25
+                result2 += squarednums*result2;
+
+                result += String.format("%d",squarednums);
+        }
+        return result;
     }
 
     public static String getRange(int start) {
-        return null;
+
+        String result ="";
+        for (int i = 0; i < start; i++) {//looping through numbers
+
+                result +=String.format("%d",i);
+
+
+        }
+        return result;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String result ="";
+        for (int i = start; i < stop; i++) {//looping through numbers
+
+            result +=String.format("%d",i);
+
+
+        }
+        return result;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result ="";
+        for (int i = start; i < stop; i+= step) {//looping through numbers
+
+            result +=String.format("%d",i);
+
+
+        }
+        return result;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result ="";
+        int result2=1;
+        int squarednums=0;
+        for (int i = start; i < stop; i += step) {//looping through numbers
+
+            squarednums = i*i;//25
+            result2 += squarednums*result2;
+
+            result += String.format("%d",squarednums);
+        }
+        return result;
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,62 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result ="";
+
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=5;j++){
+                int product = i*j;
+                if (product<10){
+                    System.out.print("   "+product);
+                }
+                else{
+                    System.out.print(" "+product);
+                }
+            }
+            System.out.println();
+        }
+
+
+        return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result ="";
+
+        for (int i=1;i<=10;i++){
+            for (int j=1;j<=10;j++){
+                int product = i*j;
+                if (product<10){
+                    System.out.print("   "+product);
+                }
+                else{
+                    System.out.print(" "+product);
+                }
+            }
+            System.out.println();
+        }
+
+
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result ="";
+
+        for (int i=1;i<=tableSize;i++){
+            for (int j=1;j<=20;j++){
+                int product = i*j;
+                if (product<20){
+                    System.out.print("   "+product);
+                }
+                else{
+                    System.out.print(" "+product);
+                }
+            }
+            System.out.println();
+        }
+
+
+        return result;
     }
 }
