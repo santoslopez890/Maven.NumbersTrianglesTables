886644cda1336fefab8d0c974da96ba38bebacc4
santos <santoslopez890@gmail.com>
2023-06-30 13:37:55 -0400

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,10 +3,20 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
+
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+
+
+            }
+        }
         return null;
     }
 
-
+//user gives 3
+    //1+2+3=6
+    //10
+    //
     public static String getOddNumbers(int start, int stop) {
         return null;
     }
